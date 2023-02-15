afdd76f9e4c4a73429de62fff2aa7981bbd5ee46
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-02-15 17:06:07 +0100

    creating get winner fxn

--- manual_rps.py
+++ manual_rps.py
@@ -11,6 +11,32 @@ def get_computer_choice():
 def get_user_choice():
     user_choice = input('Please select your choice in Rock, Paper, Scissors')
 
+    while True:
+        if user_choice != 'Rock' or 'Paper' or 'Scissors':
+            print('Try typing that again')
+        else:
+            break
     return user_choice
 
-get_computer_choice()
+
+def get_winner():
+
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+
+    if user_choice == 'Rock' and computer_choice == 'Paper':
+        print("You lost")
+    elif user_choice == 'Paper' and computer_choice == 'Scissors':
+        print("You lost")
+    elif user_choice == 'Scissors' and computer_choice == 'Rock':
+        print("You lost")
+    elif user_choice == computer_choice:
+        print("It's a tie!")
+    else:
+        print("You won!")
+
+    return
+
+    
+
+
